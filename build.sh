8326aec0774205d3ed0d395c1a3aebd4480fcac7
kevall474 <kevall474@tuta.io>
2022-01-20 21:29:40 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CARCH="aarch64"
+
 cd linux-aarch64-edge-cross-build && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 cd linux-manjaro-aarch64-edge-cross-build && makepkg -s && rm -rf pkg/ src/ && cd ..
